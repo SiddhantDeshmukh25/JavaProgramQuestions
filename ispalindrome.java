92df342f77e2681d0a25c71eddc8283c5a9ec742
Siddhant Deshmukh <siddhantdeshmukh846@gmail.com>
2025-10-24 17:12:40 +0530

    palindrome

---
+++ ispalindrome.java
@@ -0,0 +1,18 @@
+public class ispalindrome {
+    public static void main(String[] args) {
+        String str= "Siddhant";
+        System.out.println(ispalindrome(str));
+    }
+    public static boolean ispalindrome(String str){
+        int left =0;
+        int right=str.length()-1;
+
+        while(left < right){
+            if(str.charAt(left)!= str.charAt(right)){
+                return false;
+
+            }left++;
+            right--;
+        }return true;
+    }
+}
